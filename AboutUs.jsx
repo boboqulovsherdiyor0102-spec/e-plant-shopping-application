d6aea89e3021f40160735b94ca598b2b53d9afae
boboqulovsherdiyor0102-spec <boboqulovsherdiyor0102@gmail.com>
2026-03-19 12:37:31 +0500

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1>About Paradise Nursery</h1>
+      <p>Our mission is to provide high-quality houseplants to enhance your living space and well-being.</p>
+      <p>With a history of over 20 years, we are dedicated to bringing nature closer to you.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
